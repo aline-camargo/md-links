7ea530cdfc78b1134e644bab216691918618efc4
aline-camargo <aline97camargo@gmail.com>
2019-11-28 15:56:55 -0300

    option --validate ok e testada

--- cli.js
+++ cli.js
@@ -2,12 +2,17 @@
 const chalk = require('chalk');
 const mdLinks = require('./lib/index.js');
 
+const option = process.argv[3];
 const path = process.argv[2];
 
-mdLinks(path)
+mdLinks(path, option)
   .then((response) => {
     response.forEach((el) => {
-      console.log(chalk.blue(el.href), el.text);
+      if (option) {
+        console.log(chalk.blue(el.href), chalk.yellow(el.status), el.text);
+      } else {
+        console.log(chalk.blue(el.href), el.text);
+      }
     });
   })
   .catch((err) => {
