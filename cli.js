df2edcefdbd28dfffb81af5a03cea6204d41cb26
aline-camargo <aline97camargo@gmail.com>
2019-11-30 15:28:41 -0300

    usando yargs

--- cli.js
+++ cli.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 const chalk = require('chalk');
+const yargs = require('yargs');
 const mdLinks = require('./lib/index.js');
 
-const option = process.argv[3];
+const argv = yargs
+  .command('md-links', 'Busca links em arquivos .md')
+  .option('validate', {
+    alias: 'v',
+    description: 'Retorna o código de status da requisição do link',
+    type: 'boolean',
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const option = argv.validate;
 const path = process.argv[2];
 
 mdLinks(path, option)
