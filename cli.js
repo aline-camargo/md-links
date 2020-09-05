98f1045f8d1f486b8a54c80a2a76b519a741c084
Aline Camargo <aline.camargo@raizen.com>
2020-09-05 18:47:16 -0300

    Tratando mais erros e configuração eslint

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const chalk = require('chalk');
-const yargs = require('yargs');
-const mdLinks = require('./lib/index.js');
+const chalk = require('chalk')
+const yargs = require('yargs')
+const mdLinks = require('./lib/index.js')
 
 const argv = yargs
   .command('md-links', 'Busca links em arquivos .md')
@@ -12,21 +12,21 @@ const argv = yargs
   })
   .help()
   .alias('help', 'h')
-  .argv;
+  .argv
 
-const option = argv.validate;
-const path = process.argv[2];
+const option = argv.validate
+const path = process.argv[2]
 
 mdLinks(path, option)
   .then((response) => {
     response.forEach((element) => {
       if (option) {
-        console.log(chalk.blue(element.href), chalk.yellow(element.status), element.text);
+        console.log(chalk.blue(element.href), chalk.yellow(element.status), element.text)
       } else {
-        console.log(chalk.blue(element.href), element.text);
+        console.log(chalk.blue(element.href), element.text)
       }
-    });
+    })
   })
   .catch((err) => {
-    console.log(chalk.red(err));
-  });
+    console.log(chalk.red(err))
+  })
